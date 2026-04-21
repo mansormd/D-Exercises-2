5bc9705a7017edcf0a98285f7369a583cfbddcf4
mansormd <addbrobusiness@gmail.com>
2026-04-21 07:24:58 +0300

    added a new lesson

---
+++ ex 32/dom.js
@@ -0,0 +1,13 @@
+
+
+const header = document.querySelector('#header')
+
+console.log(header);
+
+const text = document.querySelectorAll('.text')
+
+console.log(text);
+
+const buttons = document.querySelectorAll('.button')
+
+console.log(buttons);
